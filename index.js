0524edeea685cef8a9c5d473416e9bd3d8f91398
unknown <cuongnv0920@gmail.com>
2022-08-25 11:05:23 +0700

    final 25.08

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const User = require("./models/user.model");
 
 const app = express();
 const port = 7000 || process.env.port;
